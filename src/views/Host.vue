e93ef0f9b7c8ef04e1fcfe0c9a914db82c202ae7
Eddie.Laser <lasereddie00@gmail.com>
2022-01-01 10:24:22 +0100

    host

--- src/views/Host.vue
+++ src/views/Host.vue
@@ -2,14 +2,26 @@
   <section class="bigWrapper">
 
 <section class="beforeQuiz"> <!--allt innan quizen börjar -->
+  <header class="head">
+    <h1>Game ID: </h1>
+  </header>
 
   <div class="participants">
     <button class="participant" v-for="i in this.leaderBoard.nicknames.length"
     v-on:click="kickfunchere(i)"
-    v-bind:key=i> {{this.leaderBoard.nicknames[i-1]}}
-</button>
+  v-bind:key=i> {{this.leaderBoard.nicknames[i-1]}}
 
-</div>
+    </button>
+  </div>
+  <div>
+    <router-link
+    v-bind:to="'/'"
+    v-slot="{href, navigate}"
+    >
+    <button :href="href" @click="navigate" class="goBack">
+      {{uiLabels.goBack}}
+    </button></router-link>
+  </div>
 </section>
 
 
@@ -90,25 +102,37 @@ export default {
   background-color: #ff2929;
 }
 .participants{
-  width: 99vw;
-  height: 95vh;
+  height: 60vh;
   display:grid;
   grid-template-columns: repeat(9, 1fr);
-  grid-template-rows: repeat(9,1fr);
+  grid-template-rows: repeat(6,1fr);
+  border: 2px solid;
+  margin-top: 5px;
 }
 
 .head{
-  margin-left: 10px;
-  margin-right: 10px;
-  height: 150px;
+  height: 125px;
+  border-bottom: 2px solid;
 }
 
 .head > h1{
   font-size: 30px;
   position: absolute;
-  top: 10%;
+  top: 5%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+
+.goBack{
+  font-size: 2vw;
+  background-color:#455879;
+  color:white;
+  border-radius: 10px;
+  left:15%;
+  top:85%;
+  width:11.25%;
+  height: 10%;
+  position: absolute;
 }
 
 </style>
