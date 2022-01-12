f7358f1234eb1c3de7bcb363c53b6e8b90f2bd72
Sebastian <ss69109@gmail.com>
2022-01-12 09:28:42 +0100

    funtion

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -2,6 +2,7 @@
   <section class="wrapper">
   <div class="1st">{{uiLabels.first}}: {{this.leaderBoard.nicknames[0]}}
     {{this.leaderBoard.scores[0]}} {{uiLabels.points}}
+  </div>
 </section>
 </template>
 <script>
@@ -13,21 +14,27 @@ export default {
     correctAnswer: Boolean,
     uiLabels: Object
   },
-  created: function(){//put logic to get the indexes of the first 5 places and ourself
-    var outp = [];
-    for (var i = 0; i < this.leaderBoard.scores.length; i++) {
-        outp.push(i); // add index to output array
-        if (outp.length > 5) {
-            outp.sort(function(a, b) { return inp[b] - inp[a]; }); // descending sort the output array
-            outp.pop(); // remove the last index (index of smallest element in output array)
-        }
-    }
-    this.indexes=outp;
-  },
+
   data: function(){
    return{
      indexes:[]
    }
+ },
+ created: function(){//put logic to get the indexes of the first 5 places and ourself
+  this.indexes=this.findIndicesOfMax(this.leaderBoard.scores, 5)
+ },
+ methods: {
+   findIndicesOfMax: function (inp, count) {
+    var outp = [];
+    for (var i = 0; i < inp.length; i++) {
+        outp.push(i); // add index to output array
+        if (outp.length > count) {
+            outp.sort(function(a, b) { return inp[b] - inp[a]; }); // descending sort the output array
+            outp.pop(); // remove the last index (index of smallest element in output array)
+        }
+    }
+    return outp;
+}
  }
 }
 </script>
